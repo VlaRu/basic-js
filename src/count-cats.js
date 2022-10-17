84bdcf55db8e9de1f376030b174761f7836caa02
Vlada <dn210690nvs@gmail.com>
2022-10-17 18:38:39 +0200

    feat: finish code js for count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,27 +14,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(matrix) {
-  
+ function countCats(matrix) {
+  let result = [].concat(...matrix);
+  let count = 0;
+  result.filter((item)=>{if(item === '^^'){return count++}})
+  return count
 }
 
 module.exports = {
   countCats
 };
 
+// второе решение, тоже верное
+/* 
 
 function countCats(matrix) {
-  let result = [].concat(matrix[0],matrix[1],matrix[2]);
-  console.log(result);
-  let count = [];
-  count.push(result.filter((item)=>{if(item === '^^'){return item}}))
+  let count = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j]==='^^') {
+        count +=1
+      }    
+    }
+  }
   return count
 }
-
 countCats([
-  [0, 1, '^^'],
-  [0, '^^', 2],
-  ['^^', 1, 2]
- ])
+  ["^^", ".", null, 0, false, "", NaN, "^^", 2, true, "dasdas", 1],
+  [2, null, 0, 1, ".", "dasdas", true, NaN, "", false, "^^", "^^"],
+  [false, ".", 1, 0, "^^", null, "", 2, "dasdas", "^^", NaN, true],
+  [".", false, 1, null, NaN, 2, 0, "dasdas", true, "^^", "", "^^"],
+  [false, ".", 1, 0, "^^", true, null, "^^", "", NaN, 2, "dasdas"],
+  [false, NaN, 1, 0, ".", "^^", null, true, "dasdas", "^^", 2, ""],
+  [null, 1, NaN, true, ".", "^^", "^^", 2, "", false, "dasdas", 0],
+  [null, NaN, "", false, ".", 1, 0, "^^", "dasdas", true, 2, "^^"],
+])
+ */
 
-
+ 
